f3c2ad5fe05d94bc2e40b48320a147807e0f4bdb
Cairo Stewart <stewartcairo@gmail.com>
2015-12-17 15:50:03 -0800

    post

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var userRoutes = require('./controllers/users.js');
+var userRoutes = require('./controllers/users');
+var morgan = require('morgan');
 
-
+app.get('/',function(req,res) { 
+  res.redirect('/users');
+ });
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-app.use('morgan');
+app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({extended: true}));
-
-
-app.get('/', function(req, res) {
-	res.redirect('/users')
-})
 
 app.use('/users', userRoutes);
 app.listen(3000, function() {
--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
+require('locus');
 
-router.get('/users',function(req,res) {
-	knex('users')
-	res.render("index");
+
+router.get('/',function(req,res) {
+	knex('users').then(function(data) {
+		res.locals.tableData = data;
+		//console.log(data)
+		res.render("pages/index");
+	})
 });
 
-modules.exports = router;
+
+router.get('/new',function(req,res) {
+  res.render('pages/new');
+});
+
+router.post('/',function(req,res) {
+  res.redirect('/users');
+  console.log(req.body)
+ });
+
+module.exports = router;
